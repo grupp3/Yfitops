dc46cc4a47ed5240bb01e6e7d541c4ac2b3992e0
Niklas Johansson <nmij78@yahoo.se>
2012-01-31 23:09:18 +0100

    Skapar nytt projekt, paket och huvudklass.
    Signed-off-by: Niklas Johansson<nmij78@yahoo.se>

---
+++ src/mainPackage/MainClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package mainPackage;
+
+/**
+ * @author Tiger
+ *
+ */
+public class MainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("grupp3rulez");
+
+	}
+
+}
